c8318309dcf0f3a46a478bd688f53db299f46fa5
wtumber <tumberw@gmail.com>
2021-12-11 20:37:03 +0000

    update model to match latest document format

--- project_code/models/Recommenders.js
+++ project_code/models/Recommenders.js
@@ -4,12 +4,8 @@ const { Schema } = mongoose;
 const recommendersSchema = new Schema(
     {
         username: {type:String, required: [true, 'User must have a username.']},
-        twitter_handle: String,
-        preferred_languages: Array,
-        brief_intro: { type: String,  maxlength: [200, "Please keep the summary short."] },
-        github_link: String,
-        number_recommendations: Number,
-        recommended_guides: Array    
+        handle: String,
+        num_reviews: Number,   
     },
     { timestamps: true }
   );
