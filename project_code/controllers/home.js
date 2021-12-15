b049090360715e4826e09d3347866d27fca2e514
wtumber <tumberw@gmail.com>
2021-12-15 13:12:01 +0000

    update homepage

--- project_code/controllers/home.js
+++ project_code/controllers/home.js
@@ -2,6 +2,7 @@ const Guides = require("../models/Guide");
 const Languages = require("../models/Language");
 const Formats = require("../models/Format");
 const Recommenders = require("../models/Recommender");
+const Language = require("../models/Language");
 
 exports.list = async (req, res) => {
     console.log(req.session);
@@ -9,11 +10,11 @@ exports.list = async (req, res) => {
 
         const activeRecommender = await Recommenders.find({}
             ).sort({num_reviews: -1}).limit(1)
-        console.log(activeRecommender) 
+
 
         
         const interactiveFormat = await Formats.findOne({content_format: "tutorial"})
-        console.log(interactiveFormat._id)
+
         const formatId = interactiveFormat._id
 
         const popularInteractiveNameRef = await Guides.aggregate([
@@ -25,33 +26,25 @@ exports.list = async (req, res) => {
             },
         ]).sort({num_reviews: -1}).limit(1)
         const popularInteractiveName = popularInteractiveNameRef[0]._id;
-        console.log(popularInteractiveNameRef[0]._id)
 
         const popularInteractive = await Guides.findOne({title: popularInteractiveName})
 
-        console.log(popularInteractive)
-        //
         
-
-        
-        const popularLanguage = await Guides.aggregate([
+        const popularLanguageRef = await Guides.aggregate([
             { $group: { 
                 _id: "$language_id",
                 num_reviews: {$sum: 1} 
                 } 
             }
-        ])
-        console.log(popularLanguage)
+        ]).sort({num_reviews: -1}).limit(1)
 
-           
+        const popularLanguageId = popularLanguageRef[0]
+        const popularLanguage = await Language.findById({_id:popularLanguageId._id})
 
-        
-
-
-        
         res.render(
             "index",
             {
+                popularLanguageReviews: popularLanguageRef[0],
                 popularLanguage: popularLanguage,
                 activeRecommender: activeRecommender[0],
                 popularInteractive: popularInteractive,
